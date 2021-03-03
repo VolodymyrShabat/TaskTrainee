aa00a0d31d4827da91bd620e9fa412959a2c934c
volodymyr.shabat <volodymyr.shabat@intenselab.com>
2021-03-03 21:33:42 +0200

    First commit

---
+++ WebApplication1/Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public bool Married { get; set; }
+
+        [Key]
+        public string Phone { get; set; }
+
+        public decimal Salary { get; set; }
+    }
+}
